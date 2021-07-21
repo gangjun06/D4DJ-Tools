06408099c22f5238978a9888610737d45494e0af
Gangjun Lee <me@gangjun.dev>
2021-07-21 22:53:25 +0900

    update

--- D4DJ.Types/MasterTypes.cs
+++ D4DJ.Types/MasterTypes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace D4DJ_Tools.Masters
 {
-    public static partial class MasterTypes
-    {
-        public static Type GetDeserializeType(string name)
-        {
-            if (specialTypes.ContainsKey(name))
-                return specialTypes[name];
+	public static partial class MasterTypes
+	{
+		public static Type GetDeserializeType(string name)
+		{
+			if (specialTypes.ContainsKey(name))
+				return specialTypes[name];
 
-            // Default to Dictionary<int, name>
-            return Type.GetType($"System.Collections.Generic.Dictionary`2[System.Int32, D4DJ_Tools.Masters.{name}]");
-        }
-    }
+			// Default to Dictionary<int, name>
+			return Type.GetType($"System.Collections.Generic.Dictionary`2[System.Int32, D4DJ_Tools.Masters.{name}]");
+		}
+	}
 }
